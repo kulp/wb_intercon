bb5f3d3767cb2ab9ab60d8fbedc48d5ce80f293c
Darren Kulp <darren@kulp.ch>
2015-07-30 17:01:02 -0500

    Name NUM_SLAVES parameter consistently in capitals

--- rtl/verilog/wb_mux.v
+++ rtl/verilog/wb_mux.v
@@ -54,9 +54,9 @@
 module wb_mux
   #(parameter dw = 32,        // Data width
     parameter aw = 32,        // Address width
-    parameter num_slaves = 2, // Number of slaves
-    parameter [num_slaves*aw-1:0] MATCH_ADDR = 0,
-    parameter [num_slaves*aw-1:0] MATCH_MASK = 0)
+    parameter NUM_SLAVES = 2, // Number of slaves
+    parameter [NUM_SLAVES*aw-1:0] MATCH_ADDR = 0,
+    parameter [NUM_SLAVES*aw-1:0] MATCH_MASK = 0)
    
    (input                      wb_clk_i,
     input 		       wb_rst_i,
@@ -75,60 +75,60 @@ module wb_mux
     output 		       wbm_err_o,
     output 		       wbm_rty_o, 
     // Wishbone Slave interface
-    output [num_slaves*aw-1:0] wbs_adr_o,
-    output [num_slaves*dw-1:0] wbs_dat_o,
-    output [num_slaves*4-1:0]  wbs_sel_o, 
-    output [num_slaves-1:0]    wbs_we_o,
-    output [num_slaves-1:0]    wbs_cyc_o,
-    output [num_slaves-1:0]    wbs_stb_o,
-    output [num_slaves*3-1:0]  wbs_cti_o,
-    output [num_slaves*2-1:0]  wbs_bte_o,
-    input [num_slaves*dw-1:0]  wbs_dat_i,
-    input [num_slaves-1:0]     wbs_ack_i,
-    input [num_slaves-1:0]     wbs_err_i,
-    input [num_slaves-1:0]     wbs_rty_i);
+    output [NUM_SLAVES*aw-1:0] wbs_adr_o,
+    output [NUM_SLAVES*dw-1:0] wbs_dat_o,
+    output [NUM_SLAVES*4-1:0]  wbs_sel_o, 
+    output [NUM_SLAVES-1:0]    wbs_we_o,
+    output [NUM_SLAVES-1:0]    wbs_cyc_o,
+    output [NUM_SLAVES-1:0]    wbs_stb_o,
+    output [NUM_SLAVES*3-1:0]  wbs_cti_o,
+    output [NUM_SLAVES*2-1:0]  wbs_bte_o,
+    input [NUM_SLAVES*dw-1:0]  wbs_dat_i,
+    input [NUM_SLAVES-1:0]     wbs_ack_i,
+    input [NUM_SLAVES-1:0]     wbs_err_i,
+    input [NUM_SLAVES-1:0]     wbs_rty_i);
 
 ///////////////////////////////////////////////////////////////////////////////
 // Master/slave connection
 ///////////////////////////////////////////////////////////////////////////////
 
-   localparam slave_sel_bits = num_slaves > 1 ? $clog2(num_slaves) : 1;
+   localparam slave_sel_bits = NUM_SLAVES > 1 ? $clog2(NUM_SLAVES) : 1;
 
    reg  			 wbm_err;
    wire [slave_sel_bits-1:0] 	 slave_sel;
-   wire [num_slaves-1:0] 	 match;
+   wire [NUM_SLAVES-1:0] 	 match;
 
    genvar 			 idx;
 
    generate
-        for(idx=0; idx<num_slaves ; idx=idx+1) begin : addr_match
+        for(idx=0; idx<NUM_SLAVES ; idx=idx+1) begin : addr_match
             assign match[idx] = ~|((wbm_adr_i ^ MATCH_ADDR[idx*aw+:aw]) & MATCH_MASK[idx*aw+:aw]);
         end
    endgenerate
 
    // priority decoder - "find first 1"
-   function ff1(input [num_slaves-1:0] match, input integer num_slaves);
+   function ff1(input [NUM_SLAVES-1:0] match, input integer num_slaves);
        integer i;
        for(i=0; i<num_slaves; i=i+1)
            if(match[i])
                ff1 = i;
    endfunction
 
-   assign slave_sel = ff1(match, num_slaves);
+   assign slave_sel = ff1(match, NUM_SLAVES);
 
    always @(posedge wb_clk_i)
      wbm_err <= wbm_cyc_i & !(|match);
 
-   assign wbs_adr_o = {num_slaves{wbm_adr_i}};
-   assign wbs_dat_o = {num_slaves{wbm_dat_i}};
-   assign wbs_sel_o = {num_slaves{wbm_sel_i}};
-   assign wbs_we_o  = {num_slaves{wbm_we_i}};
+   assign wbs_adr_o = {NUM_SLAVES{wbm_adr_i}};
+   assign wbs_dat_o = {NUM_SLAVES{wbm_dat_i}};
+   assign wbs_sel_o = {NUM_SLAVES{wbm_sel_i}};
+   assign wbs_we_o  = {NUM_SLAVES{wbm_we_i}};
 
    assign wbs_cyc_o = match & (wbm_cyc_i << slave_sel);
-   assign wbs_stb_o = {num_slaves{wbm_stb_i}};
+   assign wbs_stb_o = {NUM_SLAVES{wbm_stb_i}};
    
-   assign wbs_cti_o = {num_slaves{wbm_cti_i}};
-   assign wbs_bte_o = {num_slaves{wbm_bte_i}};
+   assign wbs_cti_o = {NUM_SLAVES{wbm_cti_i}};
+   assign wbs_bte_o = {NUM_SLAVES{wbm_bte_i}};
 
    assign wbm_dat_o = wbs_dat_i[slave_sel*dw+:dw];
    assign wbm_ack_o = wbs_ack_i[slave_sel];
